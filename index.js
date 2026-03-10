2ba853503e24d7ecb769223530ce83842d43c038
CharlotteBeecher <beechercharlotte@gmail.com>
2026-03-10 16:04:46 +0000

    removed all unnecessary files

--- index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const port = process.env.PORT;
-const app = require("./app")
-
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
